476c5994bb9c0e597fab4c902cce7d07e10d91ef
Artem Humeniuk <102350389+GreegsD@users.noreply.github.com>
2023-11-30 19:55:36 +0200

    Add files via upload

---
+++ t5.py
@@ -0,0 +1,21 @@
+'''
+Операції з числами
+Операції: + - * /
+Створіть дві змінні зі значеннями 785 і 157.
+Надрукуте по порядку результати наступних дій з ними:
+сума,
+різниця (перше мінус друге),
+множення,
+ділення (перше розділити на друге).
+'''
+
+a = 785
+b = 157
+sum_result = a + b
+difference_result = a - b
+multiplication_result = a * b
+division_result = a / b
+print("Сума:", sum_result)
+print("Різниця:", difference_result)
+print("Множення:", multiplication_result)
+print("Ділення:", division_result)
